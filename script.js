597fbab54d6a08d7c9f0565b84784f476dc8f410
Quirijn du Bois <quirijndubois@gmail.com>
2025-03-13 23:15:22 +0100

    Implemented dubble pendulum

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 import Screen from "./screen.js"
-import { Circle, Line, Polygon, Curve, FunctionGraph, LiveGrid } from "./phobject.js"
+import { Circle, Line, Polygon, Curve, FunctionGraph, LiveGrid,  Node} from "./phobject.js"
 import { Vector } from "./vector.js"
 import { Shift, Create, AnimateValue } from "./animation.js"
+import { calulateSpringForce } from "./functions.js"
 
-// just some test usage
 const canvas = document.getElementById('canvas')
 const s = new Screen(canvas)
-
-let curve1 = new FunctionGraph().setPosition(new Vector(0, 0))
 
 let grid = new LiveGrid()
 s.add(grid)
 s.addUpdater(s => grid.update(s))
 
-s.add(curve1)
+let c0 = new Node(new Vector(0,0))
+let c1 = new Node(new Vector(1,0))
+let c2 = new Node(new Vector(2,0))
 
-s.play(new Shift(curve1, new Vector(1, 0)))
+s.add(c0)
+s.add(c1)
+s.add(c2)
+s.play([new Create(c0), new Create(c1), new Create(c2)])
+
+s.addUpdater(s => {
+    const g = new Vector(0, -9.81)
+    const k = 100000
+
+    c1.addForce(g)
+    c2.addForce(g)
+
+    c1.addForce(calulateSpringForce(new Vector(0, 0), c1.position, 1, k))
+    c1.addForce(calulateSpringForce(c2.position, c1.position, 1, k))
+    c2.addForce(calulateSpringForce(c1.position, c2.position, 1, k))
+
+    c1.eulerODESover(s.dt)
+    c2.eulerODESover(s.dt)
+},100
+)
 
 s.run()
