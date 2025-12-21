8aa3f9abbe8307fe8a162bc8cb744d2d4b75180e
Quirijn du Bois <mail@quirijn.org>
2025-12-21 21:11:12 +0100

    moved library js files to src dir

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-import Screen from "./screen.js"
-import { Circle, Line, Polygon, Curve, FunctionGraph, LiveGrid,  Node} from "./phobject.js"
-import { Vector } from "./vector.js"
-import { Shift, Create, AnimateValue } from "./animation.js"
-import { calulateSpringForce } from "./functions.js"
+import Screen from "./src/screen.js"
+import { Circle, Line, Polygon, Curve, FunctionGraph, LiveGrid,  Node} from "./src/phobject.js"
+import { Vector } from "./src/vector.js"
+import { Shift, Create, AnimateValue } from "./src/animation.js"
+import { calulateSpringForce } from "./src/functions.js"
 
 const canvas = document.getElementById('canvas')
 const s = new Screen(canvas)
