f1d896bb39c748d1aa72a0107c044a2e8df27a92
Quirijn du Bois <mail@quirijn.org>
2025-12-22 00:07:55 +0100

    more compact export

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import { Animation, Shift, Create, AnimateValue, Transform} from "./animation"
+export { Animation, Shift, Create, AnimateValue, Transform} from "./animation"
 import Camera from "./camera"
-import { cloneObject, mapRange, lerp, lerp2d, qlerp, slerp, round_to_power_of_2, tupleToRGB, calulateSpringForce} from "./functions"
-import { Phobject, Graph, Circle, Line, Polygon, Curve, FunctionGraph, LiveGrid,  Node} from "./phobject"
+export { Camera }
+export { cloneObject, mapRange, lerp, lerp2d, qlerp, slerp, round_to_power_of_2, tupleToRGB, calulateSpringForce} from "./functions"
+export { Phobject, Graph, Circle, Line, Polygon, Curve, FunctionGraph, LiveGrid,  Node} from "./phobject"
 import Screen from "./screen"
-import { Vector } from "./vector"
-
-export {
-    Animation, Shift, Create, AnimateValue, Transform,
-    Camera,
-    cloneObject, mapRange, lerp, lerp2d, qlerp, slerp, round_to_power_of_2, tupleToRGB, calulateSpringForce,
-    Phobject, Graph, Circle, Line, Polygon, Curve, FunctionGraph, LiveGrid,  Node,
-    Screen, Vector
-}
+export { Screen }
+export { Vector } from "./vector"
