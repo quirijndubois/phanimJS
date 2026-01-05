cb8063bcd7352efb8368d427014f966698176fb0
Quirijn du Bois <mail@quirijn.org>
2026-01-05 18:52:15 +0100

    Refactor: seperated out some class for more generality

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-export { Animation, Shift, Create, AnimateValue, Transform} from "./animation"
+export { Animation, Shift, Create, AnimateValue, Transform } from "./animation"
 import Camera from "./camera"
 export { Camera }
-export { cloneObject, mapRange, lerp, lerp2d, qlerp, slerp, round_to_power_of_2, tupleToRGB, calulateSpringForce} from "./functions"
-export { Phobject, Graph, Circle, Line, Polygon, Curve, FunctionGraph, LiveGrid, Node, Bezier} from "./phobjects/phobject"
+export { cloneObject, mapRange, lerp, lerp2d, qlerp, slerp, round_to_power_of_2, tupleToRGB, calulateSpringForce } from "./functions"
+export { Phobject, Graph, Circle, Line, Rectangle, Polygon, Curve, FunctionGraph, LiveGrid, Node, Bezier } from "./phobjects/phobject"
 import Screen from "./screen"
 export { Screen }
 export { Vector } from "./vector"
+export { Wire, Circuit } from "./phobjects/circuit"
